410d6a6075c5736484d7be98d986eed486b73f15
alex <mcdodik2008@gmail.com>
2024-11-17 12:12:13 +0500

    Доработал логическую структуру пакетов

--- controldetecting/main.py
+++ controldetecting/main.py
@@ -1,9 +1,12 @@
 from ultralytics.utils.torch_utils import select_device
 
-from controldetecting.BehaviorClassifier import BehaviorClassifier
-from controldetecting.CapProcessor import CapProcessor
-from controldetecting.PersonDetectorYOLOv11 import PersonDetectorYOLOv11
-from controldetecting.VideoBehaviorAnalyzer import VideoAnalyzer
+from BehaviorClassifier import BehaviorClassifier
+from CapProcessor import CapProcessor
+from PersonDetectorYOLOv11 import PersonDetectorYOLOv11
+from VideoBehaviorAnalyzer import VideoAnalyzer
+
+input_video = "rutube_d/baza3.mp4"
+output_video = "results/output_video_with_behavior.mp4"
 
 detection_model_name = "microsoft/xclip-base-patch32"
 device = select_device("cuda")
@@ -13,4 +16,4 @@ cap_processor = CapProcessor(detection_model_name, device)
 classifier = BehaviorClassifier(cap_processor, device, detection_model_name)
 analyzer = VideoAnalyzer(detector, classifier, cap_processor)
 
-analyzer.analyze_video("detecting/rescaled/baza3.mp4", "results/output_video_with_behavior.mp4", False)
+analyzer.analyze_video(input_video, output_video, False)
