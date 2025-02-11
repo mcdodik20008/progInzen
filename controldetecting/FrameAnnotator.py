6db52beaf12940e2f3448cf5b06c178859b9df0d
alex <mcdodik2008@gmail.com>
2025-02-11 11:49:48 +0500

    Добавляю в конец все изображение

--- controldetecting/FrameAnnotator.py
+++ controldetecting/FrameAnnotator.py
@@ -9,11 +9,7 @@ class FrameAnnotator:
 
     def __call__(self, frame, boxes, data: AnnotateData):
         self.annotator = Annotator(frame, line_width=2, font_size=10, pil=False)
-
-        n = 0
-        len_boxes = len(boxes)
         for (box, (text, color)) in zip(boxes, data):
-            n += 1
             if box[0] == 0 and box[1] == 0:
                 print(f"ОБЩАЯ ОЦЕНКА ОБСТАНОВКИ НА ВИДЕО: {text}, размер кадра: {box}")
             self.annotator.box_label(box, text, color=color)
